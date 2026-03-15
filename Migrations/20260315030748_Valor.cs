ef47d6c629bc48dc3017ab4bb464426e70c77a19
enzoonuki <enzonuki@gmail.com>
2026-03-15 00:08:11 -0300

    migrations

---
+++ Migrations/20260315030748_Valor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIseverino.Migrations
+{
+    /// <inheritdoc />
+    public partial class Valor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ValorDeLance",
+                table: "Comentarios",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValorDeLance",
+                table: "Comentarios");
+        }
+    }
+}
