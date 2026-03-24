c39d24962f3ac1453bc962e72aa84083fe16510d
RafaRAR <rafaelroberto2005@hotmail.com>
2026-03-23 22:36:42 -0300

    ARRUMADO PORRA

---
+++ Migrations/20260324012947_fileid.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIseverino.Migrations
+{
+    /// <inheritdoc />
+    public partial class fileid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagemFileId",
+                table: "Posts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagemFileId",
+                table: "Cadastros",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagemFileId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "ImagemFileId",
+                table: "Cadastros");
+        }
+    }
+}
