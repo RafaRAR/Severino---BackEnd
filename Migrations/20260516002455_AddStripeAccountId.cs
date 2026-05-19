bad44461860ee4630a4dd908cc00122077f08861
raposo8 <victinhorolim@gmail.com>
2026-05-19 17:14:22 -0300

    commiting stripe migrations

---
+++ Migrations/20260516002455_AddStripeAccountId.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIseverino.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStripeAccountId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StripeAccountId",
+                table: "Usuarios",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StripeAccountId",
+                table: "Cadastros",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StripeAccountId",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "StripeAccountId",
+                table: "Cadastros");
+        }
+    }
+}
